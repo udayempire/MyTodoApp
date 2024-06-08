563db02aee0e286b5958634b8f5bfbebb9c796c1
udayempire <kuday0726@gmail.com>
2024-06-08 16:57:21 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const {createTodo, updateTodo} = require("./types")
+const {todo}= require("./db")
+const app = express();
+
+app.use(express.json());
+
+app.post("/todo",async function(req,res){
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload)
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg:"Oops! You have entered wrong inputs"
+        })
+        return;
+    } 
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description
+    })
+    res.json({
+        msg:"Congratulation Todo has been added"
+    })
+
+})
+
+app.get("/todos",async function(req,res){
+    const todos = await todo.find({});
+    res.json({
+        todos
+    }) 
+
+})
+
+app.get("/completed",async function(req,res){
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload)
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg:"Oops! You have entered wrong input"
+        })
+        return;
+    }
+    await todo.update(
+        {
+            _id:req.body.id
+        },
+        {
+            completed:true
+        })
+        res.json({
+            msg:"Todo marked as completed"
+        })
+})
+
+app.listen(3000)
